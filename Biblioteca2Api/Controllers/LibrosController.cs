2287c590d9fae42c358ac2aeba8be04db4d22d1b
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2024-10-15 00:31:40 -0400

    Agregando Controllers

---
+++ Biblioteca2Api/Controllers/LibrosController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Biblioteca2Api.DAL;
+using BibliotecaLib.Models;
+
+namespace Biblioteca2Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibrosController : ControllerBase
+    {
+        private readonly Contexto _context;
+
+        public LibrosController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Libros
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Libros>>> GetLibros()
+        {
+            return await _context.Libros.ToListAsync();
+        }
+
+        // GET: api/Libros/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Libros>> GetLibros(int id)
+        {
+            var libros = await _context.Libros.FindAsync(id);
+
+            if (libros == null)
+            {
+                return NotFound();
+            }
+
+            return libros;
+        }
+
+        // PUT: api/Libros/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLibros(int id, Libros libros)
+        {
+            if (id != libros.LibroId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(libros).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LibrosExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Libros
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Libros>> PostLibros(Libros libros)
+        {
+            _context.Libros.Add(libros);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLibros", new { id = libros.LibroId }, libros);
+        }
+
+        // DELETE: api/Libros/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLibros(int id)
+        {
+            var libros = await _context.Libros.FindAsync(id);
+            if (libros == null)
+            {
+                return NotFound();
+            }
+
+            _context.Libros.Remove(libros);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool LibrosExists(int id)
+        {
+            return _context.Libros.Any(e => e.LibroId == id);
+        }
+    }
+}
