abd1a771c8d21ffdeafd52f9c2abe13e9223bb45
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2024-10-14 23:58:23 -0400

    Agregando El modelo

---
+++ BibliotecaLib/Models/Libro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaLib.Models
+{
+    public class Libro
+    {
+        [Key]
+
+        public int LibroId { get; set; }
+        [Required(ErrorMessage = "Este Campo es Obligatorio")]
+
+        public string? Titulo { get; set; }
+        [Required(ErrorMessage = "Este Campo es Obligatorio")]
+        public string? Autor { get; set; }
+        [Required(ErrorMessage = "Este Campo es Obligatorio")]
+        public string? Genero { get; set; }
+        [Required(ErrorMessage = "Este Campo es Obligatorio")]
+        public bool EstaPrestado { get; set; }
+
+
+    }
+}
